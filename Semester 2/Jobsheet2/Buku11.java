d3c36c96b73f020c641eed3e698485aac019b2e3
Golden Christian Gondo <145089403+Golden028@users.noreply.github.com>
2024-02-23 17:02:39 +0700

    commit

--- Semester 2/Jobsheet2/Buku11.java
+++ Semester 2/Jobsheet2/Buku11.java
@@ -1,24 +1,34 @@
-public class Buku11 {
-  String judul, pengarang;
-  int harga, stok, halaman;
+public class Buku11{
+    String judul, pengarang;
+    int harga, stok, halaman;
 
-  void tampilinformasi(){
-      System.out.println("Judul: "+ judul);
-      System.out.println("pengaran: "+ pengarang);
-      System.out.println("Jumlah Halaman: "+ halaman);
-      System.out.println("Sisa Stok: "+ stok);
-      System.out.println("Harga: "+ harga);
-  }
+    void tampilinformasi(){
+        System.out.println("Judul: "+ judul);
+        System.out.println("pengarang: "+ pengarang);
+        System.out.println("Jumlah Halaman: "+ halaman);
+        System.out.println("Sisa Stok: "+ stok);
+        System.out.println("Harga: "+ harga);
+    }
 
-  void terjual(int jml){
-      if (stok > 0) {
-      stok -= jml;
-      }
-  }
-  void restock(int jml){
-      stok += jml;
-  }
-  void gantiharga(int hrg){
-      harga = hrg;
-  }
+    void terjual(int jml){
+        if (stok >0){
+            stok -= jml;
+        }
+    }
+    void restock(int jml){
+        stok += jml;
+    }
+    void gantiharga(int hrg){
+        harga = hrg;
+    }
+    public Buku11(){
+
+    }
+    public Buku11(String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
 }
---
+++ Semester 2/Jobsheet2/BukuMain11.java
@@ -0,0 +1,19 @@
+public class BukuMain11 {
+  public static void main(String[] args) {
+    Buku11 bk1 = new Buku11();
+    bk1.judul = "Today Ends Tomorrow Comes";
+    bk1.pengarang = "Denanda Pratiwi";
+    bk1.halaman = 198;
+    bk1.stok = 13;
+    bk1.harga = 71000;
+
+    bk1.tampilinformasi();
+    bk1.terjual(5);
+    bk1.gantiharga(60000);
+    bk1.tampilinformasi();
+
+    Buku11 bk2 = new Buku11("Self Reward","Maheera Ayesha",160,29,59000);
+    bk2.terjual(11);
+    bk2.tampilinformasi();
+  }
+}
