82861dca45ebacd1574788fca3261881b1d6aa5b
Golden Christian Gondo <145089403+Golden028@users.noreply.github.com>
2024-02-23 15:11:56 +0700

    commit

---
+++ Semester 2/Jobsheet1/Pemilihan11.java
@@ -0,0 +1,24 @@
+public class Pemilihan11 {
+  String judul, pengarang;
+  int harga, stok, halaman;
+
+  void tampilinformasi(){
+      System.out.println("Judul: "+ judul);
+      System.out.println("pengaran: "+ pengarang);
+      System.out.println("Jumlah Halaman: "+ halaman);
+      System.out.println("Sisa Stok: "+ stok);
+      System.out.println("Harga: "+ harga);
+  }
+
+  void terjual(int jml){
+      if (stok > 0) {
+      stok -= jml;
+      }
+  }
+  void restock(int jml){
+      stok += jml;
+  }
+  void gantiharga(int hrg){
+      harga = hrg;
+  }
+}
